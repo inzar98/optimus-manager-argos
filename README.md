1a23ad9396f63c6089f6c9d05b47d34e8d70748d
inzar98 <m.yaman9898@gmail.com>
2018-12-30 23:58:09 +0300

    First Branch

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# optimus-manager-argos
-An Argos Script For Optimus-Manager
+# Argos Script For Optimus-Manager
+Gnome Shell Indicator for Optimus-Manager, using Argos API
+
+Special Thanks to Cyberalex4 and Askannz <3!
+
+![NVidia Prime Argos Indicator](https://github.com/inzar98/optimus-manager-argos/blob/master/screenshots/optimus-manager-1.jpg)                                       
+
+#### Requirements
+- [Argos](https://extensions.gnome.org/extension/1176/argos/) Gnome Shell extension.
+- [Optimus-Manager](https://github.com/Askannz/optimus-manager) Awesome Optimus Switch script!
+- Any Arch based distro.
+
+
+#### Installation
+Install [Argos](https://extensions.gnome.org/extension/1176/argos/) Gnome-Shell extension.
+
+Create directory `~/.local/share/icons` if it does not exist:
+```
+! [ -d "~/.local/share/icons" ] && mkdir --parents ~/.local/share/icons || trueg
+```
+
+Then:
+```
+git clone https://github.com/inzar98/optimus-manager-argos.git
+cd argos-indicator-nvidia-prime
+
+# copy icons
+cp -v icons/* ~/.local/share/icons/
+
+# copy 'optimus-manager-ar.sh' to 'argos' folder
+cp -v optimus-manager-ar.sh ~/.config/argos/
+
+# Thats All !
+```
+#### Uninstall
+```
+# remove icons
+rm ~/.local/share/icons-to-delete/{nvidia-active-symbolic.svg,nvidia-inactive-symbolic.svg,prime-indicator-intel.svg,prime-indicator-intel-symbolic.svg,prime-indicator-nvidia.svg}
+
+
+# remove argos extension script
+rm ~/.config/argos/optimus-manager-ar.sh
+
+
+
---
+++ optimus-manager-ar.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# Thanks for idea and base script cyberalex4life <3 
+nvidia_switch="optimus-manager --no-confirm --switch nvidia"
+intel_switch="optimus-manager --no-confirm --switch intel"
+notify_switch="notify-send -h int:transient:2 -i \\\"dialog-information-symbolic\\\" \\\"Optimus Manager Indicator\\\" \\\"Switching graphics and restaring X server to finalize process! \\\" ; "
+notify_reboot="notify-send -h int:transient:2 -i \\\"dialog-information-symbolic\\\"  \\\"Optimus Manager Indicator\\\" \\\"System will reboot! \\\" ; "
+
+activate_intel="\"\
+	$notify_switch \
+	sleep 2; \
+	$intel_switch \
+	\""
+
+activate_nvidia="\"\
+	$notify_switch \
+	sleep 2; \
+	$nvidia_switch \
+	\""
+
+reboot_cmd="\"\
+	$notify_reboot \
+	sleep 1; \
+	systemctl --no-wall reboot \
+	\""
+
+nvidia_settings="\"nvidia-settings -p 'PRIME Profiles'\""
+nvidia_active_icon="nvidia-active-symbolic"
+nvidia_inactive_icon="nvidia-inactive-symbolic"
+reboot_icon="system-restart"
+
+
+QUERY=$(optimus-manager --print-mode)
+if [ "$QUERY" == 'Current mode : nvidia' ]; then
+    nvidia_state_icon=$nvidia_active_icon
+    TEMP=$(nvidia-smi -q -d TEMPERATURE | grep 'GPU Current Temp' | awk '{print $5}')
+    panel_string="$TEMP\xe2\x84\x83 | "
+else
+    nvidia_state_icon=$nvidia_inactive_icon
+    panel_string=" | "
+fi
+
+echo -e "$panel_string""iconName=$nvidia_state_icon"
+echo "---"
+echo "NVIDIA PRIME Profiles| iconName=$nvidia_state_icon bash=$nvidia_settings terminal=false"
+echo "---"
+echo "Switch to INTEL | iconName='prime-indicator-intel' bash=$activate_intel terminal=false"
+echo "Switch to NVIDIA  | iconName='prime-indicator-nvidia' bash=$activate_nvidia  terminal=false"
+
+echo "---"
+echo "System Reboot | iconName=$reboot_icon bash=$reboot_cmd terminal=false"
