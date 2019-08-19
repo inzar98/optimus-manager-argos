3c2e0118a4de0b249e9580b6c5e3390bbc9d999d
chabad360 <chabad360@users.noreply.github.com>
2019-08-19 00:40:29 -0700

    Use just two icons +more
    Refactor a bit, and remove reboot option, as it's not really necessary...

--- optimus-manager-ar.sh
+++ optimus-manager-ar.sh
@@ -3,8 +3,6 @@
 nvidia_switch="optimus-manager --no-confirm --switch nvidia"
 intel_switch="optimus-manager --no-confirm --switch intel"
 notify_switch="notify-send -h int:transient:2 -i \\\"dialog-information-symbolic\\\" \\\"Optimus Manager Indicator\\\" \\\"Switching graphics and restaring X server to finalize process! \\\" ; "
-notify_reboot="notify-send -h int:transient:2 -i \\\"dialog-information-symbolic\\\"  \\\"Optimus Manager Indicator\\\" \\\"System will reboot! \\\" ; "
-
 activate_intel="\"\
 	if zenity --question --title \\\"Optimus Manager Indicator\\\" --text \\\"Restart X server to switch on Intel?\\\" --width=256; \
 	then \
@@ -27,37 +25,22 @@ activate_nvidia="\"\
 	fi \
 	\""
 
-reboot_cmd="\"\
-	if zenity --question --title \\\"Optimus Manager Indicator\\\" --text \\\"Reboot your system?\\\" --width=160; \
-	then \
-	$notify_reboot \
-	sleep 1; \
-	systemctl --no-wall reboot; \
-	else \
-	exit 1; \
-	fi \
-	\""
-
 nvidia_settings="\"nvidia-settings -p 'PRIME Profiles'\""
-reboot_icon="'system-reboot-symbolic'"
 
 
 QUERY=$(optimus-manager --print-mode | grep 'Current GPU mode' | awk '{print $5}')
 if [ "$QUERY" == 'nvidia' ]; then
-    nvidia_state_icon=primeindicatornvidia
+    nvidia_state_icon=primeindicatornvidiasymbolic
     TEMP=$(nvidia-smi -q -d TEMPERATURE | grep 'GPU Current Temp' | awk '{print $5}')
     panel_string="$TEMP\xe2\x84\x83 | "
 else
-    nvidia_state_icon=primeindicatorintel
-    panel_string=" | "
+    nvidia_state_icon=primeindicatorintelsymbolic
+    panel_string=""
 fi
 
-echo -e "$panel_string""iconName=$nvidia_state_icon"
+echo -e "$panel_string | iconName=$nvidia_state_icon"
 echo "---"
-echo "NVIDIA PRIME Profiles| iconName=$nvidia_state_icon bash=$nvidia_settings terminal=false"
+echo "NVIDIA PRIME Profiles | iconName=$nvidia_state_icon bash=$nvidia_settings terminal=false"
 echo "---"
 echo "Switch to INTEL | iconName='primeindicatorintelsymbolic' bash=$activate_intel terminal=false"
 echo "Switch to NVIDIA  | iconName='primeindicatornvidiasymbolic' bash=$activate_nvidia  terminal=false"
-
-echo "---"
-echo "System Reboot | iconName=$reboot_icon bash=$reboot_cmd terminal=false"
